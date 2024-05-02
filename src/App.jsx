31bcc29cfbfada69916c8d35e888da5922ce94c6
Owen Coombs <owencoombs03@gmail.com>
2024-05-02 10:48:36 -0400

    got the delete

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { useContext, useState } from 'react'
 import './App.css'
 import NavBar from './navbar';
 import { TaskContext } from './main';
+import unchecked from './assets/unchecked.svg'
+
+
 
 function App() {
   const [newTask, setNewTask] = useState('');
   const { state, dispatch } = useContext(TaskContext)
+
+  const removeTask = (index) => {
+    dispatch({ type: 'removeTask', index });
+    
+  };
  
   return (
     <div>
@@ -14,20 +22,31 @@ function App() {
       </div>
       <div>
         <h2>To-Do</h2>
-      <div>
-        <input 
-          value={newTask}
-          onChange={(event) => setNewTask(event.target.value)}
-        />
-        <button onClick={() => dispatch({ type: 'addTask', title: newTask })}>Add Task</button>
-      </div>
+        <div id='form'>
+          <input 
+            value={newTask}
+            onChange={(event) => setNewTask(event.target.value)}
+          />
+          <button onClick={() => dispatch({ type: 'addTask', title: newTask })}>Add Task
+          
+          </button>
+        </div>
         <div>
           {/* Map over the tasks array and display each task */}
           {state.tasks && state.tasks.map((task, index) => (
             <div key={index}>
-              <h4>{task.title}</h4>
+              <ul id='list'>
+                <li id='list' onClick={() => removeTask(index)}>
+                  {task.title}
+                </li>
+              </ul>
             </div>
           ))}
+        </div>
+        <div>
+          <h3 id='howto'>How To Use</h3>
+          <p id='howtoP'>Once you create a task <br /> you can click on the task <br /> to delete it.</p>
+          
         </div>
       </div>
     </div>
--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,8 @@ export const taskReducer = (state, action) => {
     switch(action.type){
         case 'addTask':
             return { tasks: [...state.tasks, {title: action.title}] }
+        case 'removeTask':
+            return {tasks: state.tasks.filter((task, index) => index !== action.index)}
             default:
                 throw new Error('Error')
     }
