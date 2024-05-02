e1cba3329381d24eed001c890d672cdbaabfa5f4
Owen Coombs <owencoombs03@gmail.com>
2024-05-02 16:17:29 -0400

    added confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useContext, useEffect, useState } from 'react'
-import './App.css'
+import { useContext, useEffect, useState } from 'react';
+import './App.css';
 import NavBar from './navbar';
 import { TaskContext } from './main';
-import unchecked from './assets/unchecked.svg'
-
+import unchecked from './assets/unchecked.svg';
+import realconfetti from './assets/realconfetti.gif'
 
 
 function App() {
   const [newTask, setNewTask] = useState('');
-  const { state, dispatch } = useContext(TaskContext)
-  console.log(state)
-// Load tasks from local storage
+  const [overlayVisible, setOverlayVisible] = useState(false);
+  const { state, dispatch } = useContext(TaskContext);
+
   useEffect(() => {
     const storedTasks = localStorage.getItem('tasks');
     if (storedTasks) {
-      dispatch({type: 'setTasks', tasks: JSON.parse(storedTasks) });
+      dispatch({ type: 'setTasks', tasks: JSON.parse(storedTasks) });
     }
-
   }, [dispatch]);
-// Save tasks to local storage whenever tasks state changes
+
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(state.tasks));
   }, [state.tasks]);
 
   const removeTask = (index) => {
     dispatch({ type: 'removeTask', index });
-    
   };
- 
- 
+// when clicked will remove task and show overlay
+  const handleTaskClick = (index) => {
+    setOverlayVisible(true);
+    removeTask(index);
+    // Hide overlay after 4 seconds
+    setTimeout(() => {
+      setOverlayVisible(false);
+    }, 4000);
+  };
+
   return (
     <div>
       <div>
@@ -37,35 +43,45 @@ function App() {
       <div>
         <h2>To-Do</h2>
         <div id='form'>
-          <input 
+          <input
             value={newTask}
             onChange={(event) => setNewTask(event.target.value)}
           />
-          <button onClick={() => dispatch({ type: 'addTask', title: newTask })} id='taskBtn'>Add Task
-          
+          <button
+            onClick={() => dispatch({ type: 'addTask', title: newTask })}
+            id='taskBtn'
+          >
+            Add Task
           </button>
         </div>
         <div>
-          {/* Map over the tasks array and display each task */}
-          {state.tasks && state.tasks.map((task, index) => (
-            <div key={index}>
-              <ul id='list'>
-                <li id='list' onClick={() => removeTask(index)}>
-                  {task.title}
-                </li>
-              </ul>
-            </div>
-          ))}
+          {state.tasks &&
+            state.tasks.map((task, index) => (
+              <div key={index}>
+                <ul id='list'>
+                  <li id='list' onClick={() => handleTaskClick(index)}>
+                    {task.title}
+                  </li>
+                </ul>
+              </div>
+            ))}
         </div>
         <div>
           <h3 id='howto'>How To Use</h3>
-          <p id='howtoP'>Once you create a task <br /> you can click on the task <br /> to delete it.</p>
-          
+          <p id='howtoP'>
+            Once you create a task <br /> you can click on the task <br /> to
+            delete it.
+          </p>
         </div>
       </div>
+      {overlayVisible && (
+        <div className="overlay">
+          
+          <img src={realconfetti} alt="Overlay GIF" />
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
